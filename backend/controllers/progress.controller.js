2272943c2586477b202ef26560adee6a1560bdd8
Devashish10112003 <devashishmorwani1011@gmail.com>
2025-05-11 15:01:36 +0530

    final changes for version v1.0.0

--- backend/controllers/progress.controller.js
+++ backend/controllers/progress.controller.js
@@ -6,17 +6,37 @@ export async function getDailyProgress(req, res) {
             return res.status(401).json({ success: false, message: "Unauthorized" });
         }
 
-        const profile = await Profile.findOne({ user: req.user._id }, "dailyProgress goals");
+        const profile = await Profile.findOne({ user: req.user._id });
 
         if (!profile) {
             return res.status(404).json({ success: false, message: "User profile not found" });
         }
 
-        res.status(200).json({ success: true, dailyProgress: profile.dailyProgress, goals:profile.goals });
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        // Check if lastUpdated is before today
+        if (profile.dailyProgress.lastUpdated < today) {
+            profile.dailyProgress = {
+                totalCalories: 0,
+                totalProtein: 0,
+                totalCarbs: 0,
+                totalFats: 0,
+                totalWater: 0,
+                lastUpdated: new Date()
+            };
+
+            await profile.save();
+        }
+
+        res.status(200).json({ 
+            success: true, 
+            dailyProgress: profile.dailyProgress, 
+            goals: profile.goals 
+        });
 
     } catch (error) {
         console.error("Error fetching daily progress:", error);
         res.status(500).json({ success: false, message: "Internal server error" });
     }
 }
-
